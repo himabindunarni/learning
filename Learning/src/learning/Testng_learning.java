57e17114f5965684d6aa51fe609a6b127b1742d0
Hima <hima.x.narni@healthpartners.com>
2017-09-28 15:22:35 -0500

    commiting to the github repo
    commiting to the github repo

---
+++ Learning/src/learning/Testng_learning.java
@@ -0,0 +1,12 @@
+package learning;
+
+import org.testng.annotations.Test;
+
+public class Testng_learning {
+
+	@Test
+	public void learn1()
+	{
+		System.out.println("print a method .......");
+	}
+}
